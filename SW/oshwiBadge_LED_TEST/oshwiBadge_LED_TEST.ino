fd19a13aa2dc15dd3a44f3dfa778554e5f0a9b89
Alejandro Taracido <alex.tc.robotics@gmail.com>
2017-09-05 11:21:17 +0200

    New arduino LED test sketch

---
+++ SW/oshwiBadge_LED_TEST/oshwiBadge_LED_TEST.ino
@@ -0,0 +1,48 @@
+//Copied with shame by TCRobotics
+//Based on: http://www.instructables.com/id/ESP8266-with-Neopixeles/
+//you need to install "Adafruit NeoPixel" library
+
+#include <Adafruit_NeoPixel.h>
+
+// Which pin on the ESP8266 is connected to the NeoPixels?
+#define PIN            4
+// How many NeoPixels are attached to the ESP8266?
+#define NUMPIXELS      5
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+
+void setup() {
+  pixels.begin(); // This initializes the NeoPixel library.
+  Serial.begin(115200);
+  Serial.println("I´m Alive Setup");
+}
+
+void loop() {
+  for(int i=0;i<NUMPIXELS;i++)
+  {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(1,0,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+  Serial.println("  I´m Alive RED");
+  for(int i=0;i<NUMPIXELS;i++)
+  {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,1,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+  Serial.println("    I´m Alive GREEN");
+  for(int i=0;i<NUMPIXELS;i++)
+  {
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,0,1)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
+  Serial.println("      I´m Alive BLUE");
+  Serial.println("--------LOOP--------");
+}
