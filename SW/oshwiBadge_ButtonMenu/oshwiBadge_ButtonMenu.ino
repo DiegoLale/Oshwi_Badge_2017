490a41906778d9ae273024f05561a7e623a361a3
Alejandro Taracido <alex.tc.robotics@gmail.com>
2017-09-13 18:36:08 +0200

    Added flashlight mode

--- SW/oshwiBadge_ButtonMenu/oshwiBadge_ButtonMenu.ino
+++ SW/oshwiBadge_ButtonMenu/oshwiBadge_ButtonMenu.ino
@@ -1,4 +1,7 @@
 //Code by TCRobotics
+//USR button changes between five modes
+//Modes 1 to 4 have a timeout that return to mode 0
+//if you press five seconds USR button flashlight mode is activated!
 #include <Adafruit_NeoPixel.h>
 
 #define NEOPIXELPIN 4
@@ -10,11 +13,13 @@
 #define MODE2 2
 #define MODE3 3
 #define MODE4 4
+#define MODE5 5
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, NEOPIXELPIN, NEO_GRB + NEO_KHZ800);
 
 uint32_t red    = pixels.Color(255,   0,   0);
 uint32_t black  = pixels.Color(  0,   0,   0);
+uint32_t white  = pixels.Color(255, 255, 255);
 
 int functionMode = 0;
 long functionStartTime = 0;
@@ -26,23 +31,28 @@ void buttonInterrupt(){
   if((intTime - functionStartTime)>200) //avoid button bounce
   {
     functionMode++;
-    if (functionMode>4) functionMode = 0;
+    if (functionMode>4) functionMode = MODE0;
     functionStartTime = intTime;
   }
 }
 
 void setup() {
   pixels.begin();
-  pixels.setBrightness(8);
   pinMode(BUTTONPIN,INPUT);
   attachInterrupt(BUTTONPIN, buttonInterrupt, FALLING);
 }
 
 void loop() {
-  if((millis() - functionStartTime) > timeout) functionMode = 0;
-
   for(int i=0;i<NUMPIXELS;i++)
     pixels.setPixelColor(i, black);
+  pixels.setBrightness(8);
+  
+  if((millis() - functionStartTime) > timeout)
+  {
+    functionMode = MODE0;
+    if(!digitalRead(BUTTONPIN)) //FLASHLIGHT MODE!!
+      functionMode = MODE5;
+  }
 
   switch(functionMode)
   {
@@ -60,6 +70,14 @@ void loop() {
       break;
     case MODE4:
       pixels.setPixelColor(4, red);
+      break;
+    case MODE5: //FLASHLIGT MODE
+      pixels.setBrightness(255);
+      pixels.setPixelColor(0, white);
+      pixels.setPixelColor(1, white);
+      pixels.setPixelColor(2, white);
+      pixels.setPixelColor(3, white);
+      pixels.setPixelColor(4, white);
       break;
   }
   pixels.show();
